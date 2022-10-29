891e85c215f639c3e39f29bc92ada210fa196990
TomasJefferson2J <115845805+TomasJefferson2J@users.noreply.github.com>
2022-10-28 19:01:40 -0500

    Add files via upload

---
+++ practica3/hola.php
@@ -0,0 +1,11 @@
+<?php
+	for ($row=1; $row <= 10; $row++) { 
+		echo "<\n";
+		for ($col=1; $col <= 10; $col++) { 
+		   $p = $col * $row;
+		   echo "<td>$p</td> \n";
+		   	}
+	  	    echo "</tr>";
+		}
+		echo "</n>";
+?>
